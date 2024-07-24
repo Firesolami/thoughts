8429a27315e6195edce662d5292d3075be791f11
Firesolami <oluwoleafolabi0@gmail.com>
2024-07-24 13:29:24 +0100

    Few changes

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const passport = require('passport');
 const favicon = require('serve-favicon');
 const flash = require('connect-flash');
 const compression = require("compression");
-const helmet = require("helmet");
 
 const indexRouter = require('./routes/index');
 
